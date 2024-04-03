f4933200fad7f5aa46af6bc107a002eff45a27c7
Musab Fiqi <50223081+mfiqi@users.noreply.github.com>
2024-04-03 01:10:01 -0400

    Added Arena Manager to Lvl 1

--- Assets/ArenaManager.cs
+++ Assets/ArenaManager.cs
@@ -1,21 +1,34 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArenaManager : MonoBehaviour
+public class ArenaManager : SerializedMonoBehaviour
 {
+    public readonly GameObject Elevator;
+    public readonly GameObject RemoveBarriers;
+
+
+    private void Start()
+    {
+        if (Elevator != null)
+            Elevator.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
         /* If Arena gameobject has no children, 
          * then all enemies in arena are dead*/
-        if (transform.childCount == 1)
+        if (transform.childCount == 0)
         {
             Debug.Log("All enemies in first arena are dead");
-            /* Call triggerAllEnemiesKilled */
-            GameObject.Find("TriggerNextLevel").SetActive(true);
+            if (Elevator != null)
+                Elevator.SetActive(true);
+            if (RemoveBarriers != null)
+                RemoveBarriers.SetActive(false);
+
             Destroy(gameObject);
-            // set bool to true
         }
     }
 }
