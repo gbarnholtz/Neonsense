86c9e9017311f5335a9248e15ed5dd4d525aba85
Iconoclass <65254940+Iconoclass@users.noreply.github.com>
2024-04-07 17:22:16 -0400

    updated level 1 and level 2 to be a bit roomier and easier

--- Assets/Scripts/LensFlareController.cs
+++ Assets/Scripts/LensFlareController.cs
@@ -9,25 +9,27 @@ namespace CameraUtils
     public class LensFlareController : MonoBehaviour
     {
         [SerializeField] Camera targetCamera;
- 
+
         void OnCameraEnd(ScriptableRenderContext _, Camera currentCamera)
         {
             //if (targetCamera == null || currentCamera == targetCamera) reqLensFlare.enabled = false;
         }
- 
+
         void OnCameraStart(ScriptableRenderContext _, Camera currentCamera)
         {
             //if (targetCamera == null || currentCamera == targetCamera) reqLensFlare.enabled = true;
         }
- 
+
         void Awake()
         {
             RenderPipelineManager.beginCameraRendering += OnCameraStart;
             RenderPipelineManager.endCameraRendering += OnCameraEnd;
         }
+
         void OnDestroy()
         {
             RenderPipelineManager.beginCameraRendering -= OnCameraStart;
             RenderPipelineManager.endCameraRendering -= OnCameraEnd;
         }
     }
+}
