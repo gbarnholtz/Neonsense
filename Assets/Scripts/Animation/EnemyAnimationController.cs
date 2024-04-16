bb19994e0df0209bffa930caf4d4f5ea86e7a1ad
Musab Fiqi <50223081+mfiqi@users.noreply.github.com>
2024-04-16 17:26:28 -0400

    Animations now work with enemies

--- Assets/Scripts/Animation/EnemyAnimationController.cs
+++ Assets/Scripts/Animation/EnemyAnimationController.cs
@@ -6,18 +6,24 @@ public class EnemyAnimationController : MonoBehaviour
 {
     private Animator animator;
 
+    private Vector3 position;
+
+    float lastX, lastZ;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        animator = gameObject.GetComponent<Animator>(); 
-    }
+        animator = gameObject.GetComponent<Animator>();
+        position = gameObject.transform.parent.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float x = animator.GetFloat("MovementX");
-        float y = animator.GetFloat("MovementY");
+
+
         animator.SetFloat("MovementY", 1.0f);
         animator.SetFloat("MovementX", 0.0f);
+    }
+
+    private void Update()
+    {
     }
 }
