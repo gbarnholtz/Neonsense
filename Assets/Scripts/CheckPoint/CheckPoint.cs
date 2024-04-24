5008ea53dba8fa2693559904899ed0c0ac72bb6d
Iconoclass <65254940+Iconoclass@users.noreply.github.com>
2024-04-24 00:37:53 -0400

    added door props. optimized checkpoint.cs

--- Assets/Scripts/CheckPoint/CheckPoint.cs
+++ Assets/Scripts/CheckPoint/CheckPoint.cs
@@ -11,8 +11,9 @@ public class CheckPoint : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            CheckPointManager.lastCheckPointPos = transform.position;
-            CheckPointManager.lastCheckPointRot = transform.rotation;
+            Transform transform1 = transform;
+            CheckPointManager.lastCheckPointPos = transform1.position;
+            CheckPointManager.lastCheckPointRot = transform1.rotation;
         }
     }
 
