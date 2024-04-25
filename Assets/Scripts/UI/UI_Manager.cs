16725fb347b26e1f449b2ad148ab18e91ba2cea8
Musab Fiqi <50223081+mfiqi@users.noreply.github.com>
2024-04-25 00:03:51 -0400

    Update UI_Manager.cs

--- Assets/Scripts/UI/UI_Manager.cs
+++ Assets/Scripts/UI/UI_Manager.cs
@@ -155,7 +155,11 @@ public class UI_Manager : MonoBehaviour
     /* Checks if reload text should popup */
     private void CheckReloadPopup(RangedWeapon weapon)
     {
-        if (weapon.description == "shotgun") return;
+        if (weapon.description == "shotgun")
+        {
+            reload_popup.SetActive(false);
+            return;
+        }
         if ((float)weapon.AmmoLoaded / (float)weapon.MagazineSize < reload_popup_percentage)
         {
             reload_popup.SetActive(true);
